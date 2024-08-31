4deb75bd375f6a7b965362ef01f5abd1bea92944
Kashyap 2210 <kash.cdac@gmail.com>
2024-09-01 01:33:57 +0530

    Changes to index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -79,7 +79,7 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "Frontend", "dist")));
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 // Routing Middlewares Start
 app.use("/api/authenticate", authenticationRoutes);
@@ -87,8 +87,17 @@ app.use("/api/dish", dishRoutes); // Uncomment when ready
 // Routing Middlewares End
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
 });
+
+console.log(
+  "Serving static files from:",
+  path.join(__dirname, "../Frontend/dist")
+);
+console.log(
+  "Serving index.html from:",
+  path.join(__dirname, "../Frontend/dist/index.html")
+);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
